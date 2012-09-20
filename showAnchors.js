105bd62fc41f7e226142f888df1c41cbf171f342
Infocatcher <infocatcher.dev.null@gmail.com>
2012-09-21 01:14:34 +0400

    Add "show over" mode and close buttons

--- showAnchors.js
+++ showAnchors.js
@@ -1,46 +1,132 @@
-// (c) Infocatcher 2009, 2011
-// version 0.1.4 - 2011-08-16
+﻿// (c) Infocatcher 2009, 2011-2012
+// version 0.2.0pre - 2012-09-21
 // Style and idea based on Web Developer Firefox extension
 
 (function() {
 var anchorClass = "__anchorsBookmarkletLink";
+var anchorLinkClass = "__anchorsBookmarkletLinkHref";
+var anchorCloseClass = "__anchorsBookmarkletClose";
+var styleId = "__anchorsBookmarkletStyle";
 
 var remove = anchorClass in window;
-if(remove)
+if(remove) {
+	var oldClickHandler = window[anchorClass];
 	delete window[anchorClass];
+}
 else {
-	window[anchorClass] = true;
-	var _baseURI;
-	var _anch = document.createElement("a");
-	var s = _anch.style;
-	_anch.style.cssText =
-		"background: #ff9 !important;\n" +
-		"outline: 1px solid #fc6 !important;\n" +
-		"color: black !important;\n" +
-		"opacity: 0.85 !important;\n" +
-		"display: inline !important;\n" +
-		"/*margin-right: 2px !important;*/\n" +
-		"font: 11px Verdana,Arial,Helvetica,sans-serif !important;\n" +
-		"border: none !important;";
-	_anch.className = anchorClass;
+	window[anchorClass] = clickHandler;
+	var showOver = confirm("Show over?");
 }
 
 function anchor() {
-	return _anch.cloneNode(true);
+	var resetStyles = {
+		display:       "inline",
+		background:    "transparent",
+		color:         "black",
+		font:          "11px/15px Verdana,Arial,Helvetica,sans-serif",
+		"text-decoration": "none",
+		border:        "none",
+		border:        "none",
+		height:        "auto",
+		margin:        0,
+		padding:       0,
+		"text-shadow": "none",
+		"min-height":  0,
+		"max-height":  "none",
+	};
+	function style(node, override) {
+		var s = [];
+		for(var p in override)
+			s.push(p + ": " + override[p] + " !important;");
+		for(var p in resetStyles)
+			if(!(p in override))
+				s.push(p + ": " + resetStyles[p] + " !important;");
+		node.style.cssText = s.join("\n");
+	}
+
+	var _s = document.createElementNS("http://www.w3.org/1999/xhtml", "span");
+	_s.className = anchorClass;
+	style(_s, {
+		position: "relative",
+	});
+
+	var block = document.createElementNS("http://www.w3.org/1999/xhtml", "span");
+	style(block, {
+		position: showOver ? "absolute" : "relative",
+		top: 0,
+		left: 0,
+		background: "#ff9",
+		outline: "1px solid #fc6",
+		padding: "1px"
+	});
+
+	var anch = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+	anch.className = anchorLinkClass;
+	style(anch, {
+	});
+	block.appendChild(anch);
+
+	var close = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+	close.className = anchorCloseClass;
+	close.appendChild(document.createTextNode("x"));
+	style(close, {
+		color: "#900",
+		padding: "1px 1px 1px 2px"
+	});
+	close.href = "javascript: void 0;";
+	block.appendChild(close);
+
+	_s.appendChild(block);
+
+	anchor = function() {
+		return _s.cloneNode(true);
+	};
+	return anchor();
+}
+function windowStyle(win, add) {
+	var d = win.document;
+	if(!add) {
+		var s = d.getElementById(styleId);
+		s && rmv(s);
+		return;
+	}
+	var s = d.createElementNS("http://www.w3.org/1999/xhtml", "style");
+	s.id = styleId;
+	s.type = "text/css";
+	s.appendChild(d.createTextNode(
+		"." + anchorClass + " { z-index: 2147483646 !important; opacity: 0.8 !important; }\n" +
+		"." + anchorClass + ":hover { z-index: 2147483647 !important; opacity: 0.95 !important; }"
+	));
+	var h = d.getElementsByTagName("head")[0] || d.documentElement;
+	h.appendChild(s);
 }
 function createAnchor(h, t) {
-	var a = anchor();
+	var s = anchor();
+	var a = s.firstChild.firstChild;
 	a.href = h;
 	a.appendChild(document.createTextNode(t));
-	return a;
+	return s;
+}
+function clickHandler(e) {
+	var trg = e.target;
+	var c = trg.className;
+	if(trg.className == anchorLinkClass) {
+		e.preventDefault();
+		e.stopPropagation();
+		prompt("Link:", trg.href);
+	}
+	else if(trg.className == anchorCloseClass) {
+		e.preventDefault();
+		e.stopPropagation();
+		rmv(trg.parentNode.parentNode);
+	}
 }
 function addAnchors(win) {
-	_baseURI = win.location.href.replace(/#.*$/, "");
-	var elts = win.document.getElementsByTagName("*"), elt;
-	var anch;
-	for(var i = elts.length - 1; i >= 0; i--) {
-		elt = elts[i];
-		anch = elt.id || elt.name;
+	var _baseURI = win.location.href.replace(/#.*$/, "");
+	var elts = win.document.getElementsByTagName("*");
+	for(var i = elts.length - 1; i >= 0; --i) {
+		var elt = elts[i];
+		var anch = elt.id || elt.name;
 		if(!anch)
 			continue;
 		anch = "#" + anch;
@@ -51,12 +137,12 @@ function removeAnchors(win) {
 	var doc = win.document;
 	if("getElementsByClassName" in doc) {
 		var anchs = doc.getElementsByClassName(anchorClass);
-		for(var i = anchs.length - 1; i >= 0; i--)
+		for(var i = anchs.length - 1; i >= 0; --i)
 			rmv(anchs[i]);
 		return;
 	}
 	var anchs = doc.getElementsByTagName("a");
-	for(var i = anchs.length - 1; i >= 0; i--) {
+	for(var i = anchs.length - 1; i >= 0; --i) {
 		anch = anchs[i];
 		if(anch.className == anchorClass)
 			rmv(anch);
@@ -67,16 +153,22 @@ function rmv(node) {
 }
 function toggleAnchors(win) {
 	beginBatch(win);
-	if(remove)
+	try { windowStyle(win, !remove) }
+	catch(e) {}
+	if(remove) {
+		win.removeEventListener("click", oldClickHandler, true);
 		removeAnchors(win);
-	else
+	}
+	else {
+		win.addEventListener("click", clickHandler, true);
 		addAnchors(win);
+	}
 	endBatch(win);
 }
 function parseWin(win) {
 	try { toggleAnchors(win); }
 	catch(e) {}
-	for(var i = 0, len = win.frames.length; i < len; i++)
+	for(var i = 0, len = win.frames.length; i < len; ++i)
 		try { parseWin(win.frames[i]); }
 		catch(e) {}
 }
