ad119279fe4566c981600b6f5348f65ef754d4bc
Infocatcher <infocatcher.dev.null@gmail.com>
2013-01-20 15:10:45 +0400

    Use contentEditable from root window

--- toggleContentEditable.js
+++ toggleContentEditable.js
@@ -1,14 +1,17 @@
 (function() {
+function getBody(doc) {
+	return doc.body || doc.documentElement;
+}
 function toggleEditable(win) {
-	var body = win.document.body;
-	body.contentEditable = body.contentEditable == "true" ? "false" : "true";
+	getBody(win.document).contentEditable = ce;
 }
 function parseWin(win) {
 	try { toggleEditable(win); }
 	catch(e) {}
-	for(var i = 0, len = win.frames.length; i < len; i++)
+	for(var i = 0, len = win.frames.length; i < len; ++i)
 		try { parseWin(win.frames[i]); }
 		catch(e) {}
 }
+var ce = String(getBody(document).contentEditable != "true");
 parseWin(window);
 })();
